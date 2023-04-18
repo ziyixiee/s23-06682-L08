637d6a535cee40ee7da1ba42e94eb8d7c542bcb1
John Kitchin <jkitchin@andrew.cmu.edu>
2023-04-18 20:53:09 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # s23-06682-L08
+
+## Installation
+    cd pkg
+    pip install .
---
+++ pkg/setup.py
@@ -0,0 +1,15 @@
+"""A package for bibtex utilities."""
+
+from setuptools import setup
+
+setup(
+    name="s23oa",
+    version="0.0.1",
+    description="OpenAlex utilities",
+    maintainer="John Kitchin",
+    maintainer_email="jkitchin@andrew.cmu.edu",
+    license="MIT",
+    packages=["s23oa"],
+    scripts=[],
+    long_description="""A set of OpenAlex utilities""",
+)
